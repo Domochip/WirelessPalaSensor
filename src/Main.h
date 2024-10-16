3c59bce952d5ac5170ab50f21fae4f0227c53812
DomoChip <domochip@hotmail.com>
2024-10-16 21:42:28 +0200

    Bump version to 4.0.5

--- src/Main.h
+++ src/Main.h
@@ -15,7 +15,7 @@
 #define APPLICATION1_DESC "DomoChip Wireless Palazzetti Sensor"
 #define APPLICATION1_CLASS WebPalaSensor
 
-#define VERSION_NUMBER "4.0.4"
+#define VERSION_NUMBER "4.0.5"
 
 #define DEFAULT_AP_SSID "WirelessPala"
 #define DEFAULT_AP_PSK "PasswordPala"
